8170e48a50454bf23e278b58d6586b3b40609111
foxhunter <kevin.floba@gmail.com>
2020-09-04 10:56:31 -0400

    Se crea el fragmento

--- app/src/main/java/com/example/apppeliculas/MainActivity.kt
+++ app/src/main/java/com/example/apppeliculas/MainActivity.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.apppeliculas.model.Hero
 import com.example.apppeliculas.model.Repository
@@ -17,22 +16,11 @@ import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
-    private var heroList = ArrayList<HeroMini>()
-    private lateinit var adapter: HeroAdapter
-    private lateinit var heroViewModel : HeroViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         Log.d("tag", "creando")
-        adapter = HeroAdapter(heroList)
-        heroRecycler.adapter = adapter
-        heroViewModel = ViewModelProvider(this).get(HeroViewModel::class.java)
-        heroViewModel.listHero.observe(this, Observer{
-            adapter.updateItems(it)
-        })
-
     }
-
 }
